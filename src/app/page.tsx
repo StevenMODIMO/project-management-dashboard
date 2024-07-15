caeb836bed699ef700ab39edba14250744b6156e
StevenMODIMO <stevepetes26@gmail.com>
2024-07-15 11:23:21 +0300

    project-management-dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <main className="bg-[#2E2E2E] h-screen w-screen">
-      <header className="text-white text-2xl text-center p-4">
-        <h1>Project Management Dashboard.</h1>
-      </header>
-    </main>
-  );
+  return <main></main>;
 }
