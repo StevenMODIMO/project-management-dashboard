de07e4348c7d435a220b183f7390dd5a7c1d542b
StevenMODIMO <stevepetes26@gmail.com>
2024-07-24 19:35:11 +0300

    project-management-dashboard

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next"
 import LoginForm from "@/components/LoginForm";
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'Login for existing accounts'
+}
 
 export default function Login() {
   return <main>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
 import SignupForm from "@/components/SignupForm";
+
+export const metadata: Metadata = {
+  title: "Signup",
+  description: "Account creation for new users.",
+};
 
 export default function Signup() {
   return (
